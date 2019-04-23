1d1b68515e20b314c5b7e909ada2b9e6ae44852a
dp <13007538903@163.com>
2019-04-23 09:17:10 +0800

    第一次提交

---
+++ src/main/java/single/Car.java
@@ -0,0 +1,14 @@
+package single;
+
+public class Car {
+
+	private String name;
+	
+	public Car(String arg) {
+		System.err.println("ππ‘Ï∆Ù∂Ø");
+	}
+	
+	public void setName(String brand) {
+		this.name = brand;
+	}
+}
---
+++ src/main/java/single/CarMain.java
@@ -0,0 +1,10 @@
+package single;
+
+public class CarMain {
+
+	public static void main(String[] args) {
+		Car car = new Car("asd");
+		car.setName("qwe");
+		System.err.println(car.toString());
+	}
+}
