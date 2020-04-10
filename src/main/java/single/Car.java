e6262df77395f25eef3a714f52d29d097621796c
dp <610392769@qq.com>
2020-04-10 16:48:05 +0800

    实验

--- src/main/java/single/Car.java
+++ src/main/java/single/Car.java
@@ -6,6 +6,11 @@ public class Car {
 	
 	public Car(String arg) {
 		System.err.println("构造启动");
+		System.err.println("构造启动");
+		System.err.println("构造启动");
+		System.err.println("构造启动");
+		System.err.println("构造启动");
+		System.err.println("构造启动");
 	}
 	
 	public void setName(String brand) {
